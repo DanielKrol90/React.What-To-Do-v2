ad153b587390cb7a295d01fb42e38a040306ec68
Daniel Król <danielkrol90@gmail.com>
2022-07-11 14:39:04 +0200

    instaling icons and creating UL functional

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,27 @@
 import './Content.css'
+import React, {useState} from 'react';
+import {FaTrashAlt} from 'react-icons/fa';
 
 const Content = () => {
+
+    const [items, setItems] = useState([
+        {id: 1, checked: false, item: "Get Up from Bed"},
+        {id: 2, checked: false, item: "East and Code"},
+        {id: 3, checked: false, item: "Go to Sleep"}
+    ]);
+
+
     return (
         <section>
-            <p>List of thing here</p>
+            <ul>
+                {items.map((item) => (
+                    <li className='item'>
+                        <input type="checkbox" checked={item.checked} />
+                    <label>{item.item}</label>
+                    <FaTrashAlt role="button" tabIndex="0" />
+                    </li>
+                ))}
+            </ul>
         </section>
     );
 }
