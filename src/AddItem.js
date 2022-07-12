15f256066230c439149d08faa42b2842f72c5ad3
Daniel Król <danielkrol90@gmail.com>
2022-07-12 14:16:30 +0200

    fixed bug - wrong spelled props

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,10 +1,9 @@
-import React from "react";
 import { FaPlus } from "react-icons/fa";
 
-const AddItem = ({newItem, setNewItem, handleSubmit}) => {
+const AddItem = ({newItem, setNewItem, handleSubmit }) => {
   return (
     <form className="addForm" onSubmit = {handleSubmit}>
-      <label htmlFor="addItem"></label>
+      <label htmlFor="addItem">Add Item</label>
       <input
         autoFocus
         id="addItem"
@@ -12,13 +11,13 @@ const AddItem = ({newItem, setNewItem, handleSubmit}) => {
         placeholder="Add Item"
         required
         value={newItem}
-        onChange={(e) =>setNewItem(e.target.value)}
+        onChange={(e) => setNewItem(e.target.value)}
       />
       <button type="submit" aria-label="Add Item">
         <FaPlus />
       </button>
     </form>
   );
-};
+}
 
 export default AddItem;
--- src/App.js
+++ src/App.js
@@ -22,29 +22,27 @@ function App() {
   };
 
   const addItem = (item) => {
-    const id = items.length ? items[items.length - 1].id +1 : 1;
-    const newItem = {id, checked: false,item};
-    const listItems = [...items, newItem];
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
     setAndSaveItems(listItems);
-  };
+  }
 
-  const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
+   const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setAndSaveItems(listItems);
-  };
+  }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
     setAndSaveItems(listItems);
-  };
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
-      if (!newItem) return;
-        addItem(newItem);
-        setNewItem('')
-  };
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem('');
+  }
 
   return (
     <div className="App">
@@ -52,7 +50,7 @@ function App() {
       <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
-        handleInput={handleSubmit}
+        handleSubmit={handleSubmit}
       />
       <Content
         items={items}
